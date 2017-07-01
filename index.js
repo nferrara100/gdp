3b3ac77b7153edf8a32a2ce1895c0a568207ca84
Nicholas Ferrara <nferrara100@gmail.com>
2017-07-01 18:49:11 -0400

    Working data collection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var Alexa = require("alexa-sdk");
 var xml2js = require('xml2js');
+var parser = new xml2js.Parser();
 var request = require('request');
 
 exports.handler = function(event, context, callback) {
@@ -18,18 +19,23 @@ var handlers = {
         this.emit('calculate')
     },
     'calculate': function() {
-        var request = require('request');
-        request({
-            url: 'http://api.worldbank.org/countries/us/indicators/NY.GDP.MKTP.CD?date=2016:2016',
-            encoding: 'utf-8',
-            callback: function (error, response, body) {
-                console.log('body:', body);
-                //parser.parseString(body);
-                }
-        });
+        request(
+            { method: 'GET'
+             , uri: 'http://api.worldbank.org/countries/us/indicators/NY.GDP.MKTP.CD?date=2016:2016'
+             , gzip: true
+            }
+            , function (error, response, body) {
+                // body is the decompressed response body
+                //console.log('the decoded data is: ' + body);
+                parser.parseString(body, function (err, result) {
+                    complete(JSON.stringify(result['wb:data']['wb:data'][0]['wb:value'][0]));
+                });
+            }
+        );
     }
 };
 
-function complete() {
-    this.emit(':tell', 'Hello new World!');
+function complete(value) {
+    console.log(value);
+    //this.emit(':tell', 'Hello new World! Data: ' + value);
 }
