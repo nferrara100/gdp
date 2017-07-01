501fdfa21206da384503b1127a17c6c7a219a040
Nicholas Ferrara <nferrara100@gmail.com>
2017-07-01 19:54:28 -0400

    Value rounding

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ var Alexa = require("alexa-sdk");
 var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
 var request = require('request');
+var countrynames = require('countrynames');
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
+
+var myAlexa;
+var country;
 
 var handlers = {
     'LaunchRequest': function() {
@@ -19,23 +23,33 @@ var handlers = {
         this.emit('calculate')
     },
     'calculate': function() {
+        myAlexa = this;
+        country = myAlexa.event.request.intent.slots.country.value;
+        var countryCode = countrynames.getCode(country);
         request(
             { method: 'GET'
-             , uri: 'http://api.worldbank.org/countries/us/indicators/NY.GDP.MKTP.CD?date=2016:2016'
+             , uri: 'http://api.worldbank.org/countries/' + countryCode + '/indicators/NY.GDP.MKTP.CD?date=2016:2016'
              , gzip: true
             }
             , function (error, response, body) {
                 // body is the decompressed response body
                 //console.log('the decoded data is: ' + body);
                 parser.parseString(body, function (err, result) {
-                    complete(JSON.stringify(result['wb:data']['wb:data'][0]['wb:value'][0]));
+                    var remoteValue = JSON.stringify(result['wb:data']['wb:data'][0]['wb:value'][0]);
+                    remoteValue = remoteValue.substring(1, remoteValue.length-1); //Removes quotes
+                    var roundedValue = precisionRound(parseFloat(remoteValue), -9);
+                    myAlexa.emit(':tell', 'The GDP of ' + country + ' is ' + roundedValue + '.');
                 });
             }
         );
     }
 };
 
-function complete(value) {
-    console.log(value);
-    //this.emit(':tell', 'Hello new World! Data: ' + value);
-}
+function precisionRound (number, precision) {
+    var factor = Math.pow(10, precision);
+    var tempNumber = number * factor;
+    var roundedTempNumber = Math.round(tempNumber);
+    return roundedTempNumber / factor;
+};
+
+9
