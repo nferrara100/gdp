6a964f06842ffea54683f54e216487aa56364f22
Nicholas Ferrara <nferrara100@gmail.com>
2017-07-01 23:52:09 -0400

    Basic complete

--- index.js
+++ index.js
@@ -9,48 +9,52 @@ exports.handler = function(event, context, callback) {
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
-try {
-    var handlers = {
-        'LaunchRequest': function() {
-            this.emit('calculate');
-        },
-        'GetCountryGDPIntent': function() {
-            this.emit('calculate')
-        },
-        'calculate': function() {
-            var myAlexa = this;
-            var country = myAlexa.event.request.intent.slots.country.value;
-            var countryShort = country.replace("the ", "")
-            var countryCode = countrynames.getCode(countryShort);
-            if (!countryCode) {
-                countryCode = countryShort;
-            }
-            request({
-                method: 'GET',
-                uri: 'http://api.worldbank.org/countries/' + countryCode + '/indicators/NY.GDP.MKTP.CD?date=2016:2016',
-                gzip: true
-            }, function(error, response, body) {
-                if (error) {
-                    myAlexa.emit(':tell', "Sorry, I'm having trouble connecting to my database. Please try again later.");
-                }
-                // body is the decompressed response body
-                console.log('the decoded data is: ' + body);
-                parser.parseString(body, function(err, result) {
-                    if (err || result['wb:error']) { //Not a good way to find out
-                        myAlexa.emit(':tell', "I couldn't find any data on " + country + ". Does it go by any other names?");
-                    } else {
-                        var remoteValue = JSON.stringify(result['wb:data']['wb:data'][0]['wb:value'][0]);
-                        remoteValue = remoteValue.substring(1, remoteValue.length - 1); //Removes quotes
-                        var roundedValue = precisionRound(parseFloat(remoteValue), -9);
-                        myAlexa.emit(':tell', 'The GDP of ' + country + ' is ' + roundedValue + '.');
-                    }
-                });
-            });
+var myAlexa;
+var helpMessage = "Ask me what the GDP is of any country in the world.";
+var handlers = {
+    'AMAZON.HelpIntent': function() {
+        myAlexa.emit(':tell', helpMessage);
+    },
+    'LaunchRequest': function() {
+        this.emit('calculate');
+    },
+    'GetCountryGDPIntent': function() {
+        this.emit('calculate')
+    },
+    'calculate': function() {
+        myAlexa = this;
+        var country = myAlexa.event.request.intent.slots.country.value;
+        var countryShort = country.replace("the ", "")
+        var countryCode = countrynames.getCode(countryShort);
+        if (!countryCode) {
+            countryCode = countryShort;
         }
-    };
-} catch (error) {
-    this.emit(':tell', "Sorry, I encountered an unexpected error. Please try again later.");
-}
+        request({
+            method: 'GET',
+            uri: 'http://api.worldbank.org/countries/' + countryCode + '/indicators/NY.GDP.MKTP.CD?date=2016:2016',
+            gzip: true
+        }, function(error, response, body) {
+            if (error) {
+                myAlexa.emit(':tell', "Sorry, I'm having trouble connecting to my database. Please try again later.");
+            }
+            // body is the decompressed response body
+            //console.log('the decoded data is: ' + body);
+            parser.parseString(body, function(err, result) {
+                if (err || result['wb:error']) { //Not a good way to find out
+                    myAlexa.emit(':tell', "I couldn't find any data on " + country + ". Does it go by any other names?");
+                } else {
+                    var remoteValue = JSON.stringify(result['wb:data']['wb:data'][0]['wb:value'][0]);
+                    remoteValue = remoteValue.substring(1, remoteValue.length - 1); //Removes quotes
+                    var roundedValue = precisionRound(parseFloat(remoteValue), -9);
+                    myAlexa.emit(':tell', 'The Gross Domestic Product of ' + country + ' is ' + roundedValue + '.');
+                }
+            });
+        });
+    },
+    'Unhandled': function() {
+        this.emit(':tell', helpMessage);
+    }
+};
 
 function precisionRound(number, precision) {
     var factor = Math.pow(10, precision);
