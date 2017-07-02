c1cfc61406173caf8eee0e4b7b4e8eb72914e1c4
Nicholas Ferrara <nferrara100@gmail.com>
2017-07-01 21:43:11 -0400

    Fully working except error handling

--- index.js
+++ index.js
@@ -1,55 +1,60 @@
 'use strict';
-
 var Alexa = require("alexa-sdk");
 var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
 var request = require('request');
 var countrynames = require('countrynames');
-
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
-
-var myAlexa;
-var country;
-
-var handlers = {
-    'LaunchRequest': function() {
-        this.emit('calculate');
-    },
-    'GetCountryGDPIntent': function() {
-        this.emit('calculate')
-    },
-    'calculate': function() {
-        myAlexa = this;
-        country = myAlexa.event.request.intent.slots.country.value;
-        var countryCode = countrynames.getCode(country);
-        request(
-            { method: 'GET'
-             , uri: 'http://api.worldbank.org/countries/' + countryCode + '/indicators/NY.GDP.MKTP.CD?date=2016:2016'
-             , gzip: true
+try {
+    var handlers = {
+        'LaunchRequest': function() {
+            this.emit('calculate');
+        },
+        'GetCountryGDPIntent': function() {
+            this.emit('calculate')
+        },
+        'calculate': function() {
+            var myAlexa = this;
+            var country = myAlexa.event.request.intent.slots.country.value;
+            var countryShort = country.replace("the ", "")
+            var countryCode = countrynames.getCode(countryShort);
+            if (!countryCode) {
+                countryCode = countryShort;
             }
-            , function (error, response, body) {
+            request({
+                method: 'GET',
+                uri: 'http://api.worldbank.org/countries/' + countryCode + '/indicators/NY.GDP.MKTP.CD?date=2016:2016',
+                gzip: true
+            }, function(error, response, body) {
+                if (error) {
+                    myAlexa.emit(':tell', "Sorry, I'm having trouble connecting to my database. Please try again later.");
+                }
                 // body is the decompressed response body
-                //console.log('the decoded data is: ' + body);
-                parser.parseString(body, function (err, result) {
-                    var remoteValue = JSON.stringify(result['wb:data']['wb:data'][0]['wb:value'][0]);
-                    remoteValue = remoteValue.substring(1, remoteValue.length-1); //Removes quotes
-                    var roundedValue = precisionRound(parseFloat(remoteValue), -9);
-                    myAlexa.emit(':tell', 'The GDP of ' + country + ' is ' + roundedValue + '.');
+                console.log('the decoded data is: ' + body);
+                parser.parseString(body, function(err, result) {
+                    if (err || result['wb:error']) { //Not a good way to find out
+                        myAlexa.emit(':tell', "I couldn't find any data on " + country + ". Does it go by any other names?");
+                    } else {
+                        var remoteValue = JSON.stringify(result['wb:data']['wb:data'][0]['wb:value'][0]);
+                        remoteValue = remoteValue.substring(1, remoteValue.length - 1); //Removes quotes
+                        var roundedValue = precisionRound(parseFloat(remoteValue), -9);
+                        myAlexa.emit(':tell', 'The GDP of ' + country + ' is ' + roundedValue + '.');
+                    }
                 });
-            }
-        );
-    }
-};
+            });
+        }
+    };
+} catch (error) {
+    this.emit(':tell', "Sorry, I encountered an unexpected error. Please try again later.");
+}
 
-function precisionRound (number, precision) {
+function precisionRound(number, precision) {
     var factor = Math.pow(10, precision);
     var tempNumber = number * factor;
     var roundedTempNumber = Math.round(tempNumber);
     return roundedTempNumber / factor;
-};
-
-9
+}
